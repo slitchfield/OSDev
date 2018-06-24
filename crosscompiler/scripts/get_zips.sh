bab4dd1cf0f49f505b6c258cfc288e1c27837b4a
Samuel Litchfield <samuel.litchfield@gmail.com>
2018-06-24 10:11:24 -0400

    Fixed a bug in get_zips.sh and added default meaty-skeleton install

--- crosscompiler/scripts/get_zips.sh
+++ crosscompiler/scripts/get_zips.sh
@@ -5,7 +5,7 @@ BINUTILS_VERSION=2.29.1
 
 GCC_URL="https://ftp.gnu.org/gnu/gcc/gcc-$GCC_VERSION/gcc-$GCC_VERSION.tar.gz"
 BINUTILS_URL="https://ftp.gnu.org/gnu/binutils/binutils-$BINUTILS_VERSION.tar.gz"
-wget -nc -P ../zips/ $GCC_URL
+wget -nc -P ./zips/ $GCC_URL
 #mv ../zips/gcc-$GCC_VERSION.tar.gz ../zips/gcc.tar.gz
-wget -nc -P ../zips/ $BINUTILS_URL
+wget -nc -P ./zips/ $BINUTILS_URL
 #mv ../zips/binutils-$BINUTILS_VERSION.tar.gz ../zips/binutils.tar.gz
--- crosscompiler/scripts/install_deps.sh
+++ crosscompiler/scripts/install_deps.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo apt install build-essential bison flex libgmp3-dev libmpc-dev libmpfr-dev texinfo libcloog-isl-dev libisl-dev
+sudo apt install build-essential bison flex libgmp3-dev libmpc-dev libmpfr-dev texinfo libcloog-isl-dev libisl-dev qemu grub-pc-bin xorriso
